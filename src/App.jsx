7b7dab1a6a17780c89b080c2c957ec6def722dd1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-06 18:30:59 -0400

    Add state to track difficulty

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import styles from "./App.module.css";
 
 function App() {
   const [scene, setScene] = useState("Title");
+  const [difficulty, setDifficulty] = useState(1);
 
   return (
     <div className={styles.appContainer}>
       <GitLinkHeader></GitLinkHeader>
       {scene === "Title" ? (
-        <TitleScene setScene={setScene}></TitleScene>
+        <TitleScene
+          setScene={setScene}
+          difficulty={difficulty}
+          setDifficulty={setDifficulty}
+        ></TitleScene>
       ) : scene === "Game" ? null : null}
     </div>
   );
