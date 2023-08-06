4b25bdda187a41db4ac68ea879b07629921788a7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-06 16:40:24 -0400

    Add file and skeleton for TitleScene component

---
+++ src/components/TitleScene/TitleScene.jsx
@@ -0,0 +1,16 @@
+function TitleScene() {
+  return (
+    <main>
+      <h1>Memory Cards</h1>
+      <button>Play</button>
+      <section>
+        <p>Difficulty</p>
+        <button>Easy - 5 Cards</button>
+        <button>Medium - 10 Cards</button>
+        <button>Hard - 15 Cards</button>
+      </section>
+    </main>
+  );
+}
+
+export default TitleScene;
