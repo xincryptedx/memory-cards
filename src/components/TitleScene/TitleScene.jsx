09f857eb38b9a9d10f42f2a4758a446c794a7c70
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-06 18:40:36 -0400

    Add class based on difficulty state

--- src/components/TitleScene/TitleScene.jsx
+++ src/components/TitleScene/TitleScene.jsx
@@ -1,15 +1,22 @@
+/* eslint-disable react/prop-types */
 import styles from "./TitleScene.module.css";
 
-function TitleScene() {
+function TitleScene({ difficulty, setDifficulty }) {
   return (
     <main className={styles.titleScene}>
       <h1 className={styles.title}>Memory Cards</h1>
       <button className={styles.playButton}>Play</button>
       <section className={styles.difficultySection}>
         <p>Select a Difficulty</p>
-        <button>Easy - 5 Cards</button>
-        <button>Medium - 10 Cards</button>
-        <button>Hard - 15 Cards</button>
+        <button className={difficulty === 0 ? styles.difficultySelected : ""}>
+          Easy - 5 Cards
+        </button>
+        <button className={difficulty === 1 ? styles.difficultySelected : ""}>
+          Medium - 10 Cards
+        </button>
+        <button className={difficulty === 2 ? styles.difficultySelected : ""}>
+          Hard - 15 Cards
+        </button>
       </section>
     </main>
   );
