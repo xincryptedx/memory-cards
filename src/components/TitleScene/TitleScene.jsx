72d031ebe0126000aa1296166b110221b35798f0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-06 18:45:04 -0400

    Set difficulty when buttons clicked

--- src/components/TitleScene/TitleScene.jsx
+++ src/components/TitleScene/TitleScene.jsx
@@ -8,13 +8,22 @@ function TitleScene({ difficulty, setDifficulty }) {
       <button className={styles.playButton}>Play</button>
       <section className={styles.difficultySection}>
         <p>Select a Difficulty</p>
-        <button className={difficulty === 0 ? styles.difficultySelected : ""}>
+        <button
+          onClick={() => setDifficulty(0)}
+          className={difficulty === 0 ? styles.difficultySelected : ""}
+        >
           Easy - 5 Cards
         </button>
-        <button className={difficulty === 1 ? styles.difficultySelected : ""}>
+        <button
+          onClick={() => setDifficulty(1)}
+          className={difficulty === 1 ? styles.difficultySelected : ""}
+        >
           Medium - 10 Cards
         </button>
-        <button className={difficulty === 2 ? styles.difficultySelected : ""}>
+        <button
+          onClick={() => setDifficulty(2)}
+          className={difficulty === 2 ? styles.difficultySelected : ""}
+        >
           Hard - 15 Cards
         </button>
       </section>
