91f8282679bf921f725ab94e6e20ee9522662b3e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-06 16:47:41 -0400

    Add conditional render logic for scenes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import GitLinkHeader from "./components/GitLinkHeader/GitLinkHeader";
+import TitleScene from "./components/TitleScene/TitleScene";
 import styles from "./App.module.css";
 
 function App() {
+  const [scene, setScene] = useState("Title");
+
   return (
     <div className={styles.appContainer}>
       <GitLinkHeader></GitLinkHeader>
+      {scene === "Title" ? (
+        <TitleScene setScene={setScene}></TitleScene>
+      ) : scene === "Game" ? null : null}
     </div>
   );
 }
