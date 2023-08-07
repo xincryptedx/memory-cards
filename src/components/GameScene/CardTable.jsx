b6f324bdc5bad82297625214173609fbd3d4e70d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 15:17:45 -0400

    Pass random power and suit as props to cards

--- src/components/GameScene/CardTable.jsx
+++ src/components/GameScene/CardTable.jsx
@@ -1,22 +1,45 @@
+/* eslint-disable react/prop-types */
 import styles from "./CardTable.module.css";
 import Card from "./Card.jsx";
 
-function CardTable() {
-  return (
-    <section className={styles.cardTable}>
-      {/* Cards get dynamically added here */}
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-    </section>
-  );
+const powers = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+];
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+const getRandomEntry = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
+function CardTable({ difficulty }) {
+  const getRandomCards = () => {
+    let cards = [];
+    for (let i = 0; i < difficulty; i += 1) {
+      cards.push(
+        <Card
+          key={i}
+          power={getRandomEntry(powers)}
+          suits={getRandomEntry(suits)}
+        ></Card>
+      );
+    }
+    return cards;
+  };
+
+  return <section className={styles.cardTable}>{getRandomCards()}</section>;
 }
 
 export default CardTable;
