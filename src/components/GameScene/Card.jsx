bb7343a2d6c152bc84e216b40b836be90caf21d3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 16:02:19 -0400

    Add effect for fetching card image using deck of cards api

--- src/components/GameScene/Card.jsx
+++ src/components/GameScene/Card.jsx
@@ -1,12 +1,25 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import styles from "./Card.module.css";
 
 function Card({ power, suit }) {
+  const [image, setImage] = useState("");
+
   const name = `${power} of ${suit}`;
+
+  useEffect(() => {
+    const cardCode = `${power[0]}${suit[0]}`.toUpperCase();
+
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?cards=${cardCode}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.cards[0].image);
+      });
+  }, [power, suit]);
 
   return (
     <div className={styles.card}>
-      <img src="" alt={name} />
+      <img src={image} alt={name} />
     </div>
   );
 }
