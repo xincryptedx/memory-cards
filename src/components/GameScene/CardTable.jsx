cf6302abcbae378982dddf9e90376e6d2f5645e8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 13:51:55 -0400

    Remove wrapper div

--- src/components/GameScene/CardTable.jsx
+++ src/components/GameScene/CardTable.jsx
@@ -3,21 +3,19 @@ import Card from "./Card.jsx";
 
 function CardTable() {
   return (
-    <div className={styles.scrollWrapper}>
-      <section className={styles.cardTable}>
-        {/* Cards get dynamically added here */}
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-      </section>
-    </div>
+    <section className={styles.cardTable}>
+      {/* Cards get dynamically added here */}
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </section>
   );
 }
 
