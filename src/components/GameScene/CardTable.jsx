85afedd5132e3fad55e11812a16d579b57b1dee2
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 12:36:24 -0400

    Add component skeleton

---
+++ src/components/GameScene/CardTable.jsx
@@ -0,0 +1,11 @@
+import styles from "./CardTable.module.css";
+
+function CardTable() {
+  return (
+    <section className={styles.cardTable}>
+      {/* Cards get dynamically added here */}
+    </section>
+  );
+}
+
+export default CardTable;
