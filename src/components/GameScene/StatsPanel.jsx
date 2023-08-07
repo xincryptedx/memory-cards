1e230c11cbb134b8537908b0358fb6a705a1260b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 14:20:32 -0400

    Add classes for styling stats

--- src/components/GameScene/StatsPanel.jsx
+++ src/components/GameScene/StatsPanel.jsx
@@ -3,13 +3,13 @@ import styles from "./StatsPanel.module.css";
 function StatsPanel() {
   return (
     <section className={styles.statsPanel}>
-      <p>
+      <p className={styles.round}>
         Round: <span></span>
       </p>
-      <p>
+      <p className={styles.score}>
         Score: <span></span>
       </p>
-      <p>
+      <p className={styles.highScore}>
         High Score: <span></span>
       </p>
     </section>
