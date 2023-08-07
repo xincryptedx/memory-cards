29d73537fcb20134d6de01ce9d989136b15ad2ba
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 15:00:15 -0400

    Add suit and power information and method for getting random array entry

--- src/components/GameScene/GameScene.jsx
+++ src/components/GameScene/GameScene.jsx
@@ -3,6 +3,28 @@ import ControlPanel from "./ControlPanel";
 import styles from "./GameScene.module.css";
 import StatsPanel from "./StatsPanel";
 
+const powers = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+];
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+const getRandomEntry = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
 function GameScene() {
   return (
     <main className={styles.gameScene}>
