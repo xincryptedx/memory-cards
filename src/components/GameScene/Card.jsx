22fbe6e8aafc44f6df881a173bb86af1ec61d8b3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 12:40:58 -0400

    Add component skeleton

---
+++ src/components/GameScene/Card.jsx
@@ -0,0 +1,11 @@
+import styles from "./Card.module.css";
+
+function Card() {
+  return (
+    <div className={styles.card}>
+      <img src="" alt="Playing Card - Power of Suit" />
+    </div>
+  );
+}
+
+export default Card;
