07bdb57c63c974218aff135e573d7338a946199d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 12:54:26 -0400

    Import Card and add some Cards for layot testing

--- src/components/GameScene/CardTable.jsx
+++ src/components/GameScene/CardTable.jsx
@@ -1,9 +1,20 @@
 import styles from "./CardTable.module.css";
+import Card from "./Card.jsx";
 
 function CardTable() {
   return (
     <section className={styles.cardTable}>
       {/* Cards get dynamically added here */}
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
     </section>
   );
 }
