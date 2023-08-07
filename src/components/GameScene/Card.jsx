a782aa476c8beff213449ff2df741578e36a620a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 15:20:18 -0400

    Set alt text to card name

--- src/components/GameScene/Card.jsx
+++ src/components/GameScene/Card.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/prop-types */
 import styles from "./Card.module.css";
 
-function Card() {
+function Card({ power, suit }) {
+  const name = `${power} of ${suit}`;
+
   return (
     <div className={styles.card}>
-      <img src="" alt="Playing Card - Power of Suit" />
+      <img src="" alt={name} />
     </div>
   );
 }
