4f235d0ca95a477a402a0b009439075b0a954a5f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 11:50:55 -0400

    Add skeleton to lay out needed components

---
+++ src/components/GameScene/GameScene.jsx
@@ -0,0 +1,13 @@
+import styles from "./GameScene.module.css";
+
+function GameScene() {
+  return (
+    <main className={styles.gameScene}>
+      {/* Controls */}
+      {/* Stats */}
+      {/* CardTable */}
+    </main>
+  );
+}
+
+export default GameScene;
