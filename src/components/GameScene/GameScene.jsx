b16673a1a273e002dd455b260b301b40f4eef6b9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 15:07:53 -0400

    Move data and helper out and pass difficulty

--- src/components/GameScene/GameScene.jsx
+++ src/components/GameScene/GameScene.jsx
@@ -1,36 +1,15 @@
+/* eslint-disable react/prop-types */
 import CardTable from "./CardTable";
 import ControlPanel from "./ControlPanel";
 import styles from "./GameScene.module.css";
 import StatsPanel from "./StatsPanel";
 
-const powers = [
-  "Ace",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "Jack",
-  "Queen",
-  "King",
-];
-const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-
-const getRandomEntry = (array) => {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-};
-
-function GameScene() {
+function GameScene({ difficulty }) {
   return (
     <main className={styles.gameScene}>
       <ControlPanel></ControlPanel>
       <StatsPanel></StatsPanel>
-      <CardTable></CardTable>
+      <CardTable difficulty={difficulty}></CardTable>
     </main>
   );
 }
