602a5a993f16a28c19f797571c1b1b710d0d2037
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-07 12:31:19 -0400

    Import and add StatsPanel

--- src/components/GameScene/GameScene.jsx
+++ src/components/GameScene/GameScene.jsx
@@ -1,11 +1,12 @@
 import ControlPanel from "./ControlPanel";
 import styles from "./GameScene.module.css";
+import StatsPanel from "./StatsPanel";
 
 function GameScene() {
   return (
     <main className={styles.gameScene}>
       <ControlPanel></ControlPanel>
-      {/* Stats */}
+      <StatsPanel></StatsPanel>
       {/* CardTable */}
     </main>
   );
