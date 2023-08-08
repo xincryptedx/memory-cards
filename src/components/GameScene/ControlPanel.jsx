5be9e5ace7267f8996b8177fb514ef3412379728
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-08 11:36:39 -0400

    Add method for quit event handling

--- src/components/GameScene/ControlPanel.jsx
+++ src/components/GameScene/ControlPanel.jsx
@@ -1,10 +1,20 @@
+/* eslint-disable react/prop-types */
 import styles from "./ControlPanel.module.css";
 
-function ControlPanel() {
+function ControlPanel({ setCards, setScore, setRound, setScene }) {
+  const onQuit = () => {
+    setCards([]);
+    setScore(0);
+    setRound(1);
+    setScene("Title");
+  };
+
   return (
     <nav className={styles.controlPanel}>
       <button className={styles.newGameButton}>New Game</button>
-      <button className={styles.quitButton}>Quit</button>
+      <button className={styles.quitButton} onClick={onQuit}>
+        Quit
+      </button>
     </nav>
   );
 }
