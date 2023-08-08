d977aa2e935b5c5fa2535da55e6f770fc5630352
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-08 12:58:02 -0400

    Use correct card code for api

--- src/code/useCardImage.js
+++ src/code/useCardImage.js
@@ -2,18 +2,19 @@ import { useState, useEffect } from "react";
 
 function useCardImage(power, suit) {
   const [image, setImage] = useState();
+  const cardCode = `${power[0]}${suit[0]}`.toUpperCase();
 
   useEffect(() => {
     const fetchImage = async () => {
       const response = await fetch(
-        `https://deckofcardsapi.com/api/deck/new/draw/?cards=${power[0]}${suit[0]}`
+        `https://deckofcardsapi.com/api/deck/new/draw/?cards=${cardCode}`
       );
       const data = await response.json();
       setImage(data.cards[0].image);
     };
 
     fetchImage();
-  }, [power, suit]);
+  }, [cardCode]);
 
   return image;
 }
