6a32377ab9ecd6bd30603f7da4faf67913b09027
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-08 15:06:37 -0400

    Remove effect

--- src/code/useRandomCards.js
+++ src/code/useRandomCards.js
@@ -1,21 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function useRandomCards(difficulty) {
-  const [cardsData, setCardsData] = useState();
+  const [cards, setCards] = useState([]);
+  const fetchCards = async () => {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/new/draw/?count=${difficulty}`
+    );
+    const returnData = await response.json();
+    setCards(returnData.cards);
+  };
 
-  useEffect(() => {
-    const fetchCards = async () => {
-      const response = await fetch(
-        `https://deckofcardsapi.com/api/deck/new/draw/?count=${difficulty}`
-      );
-      const { cards } = await response.json();
-      setCardsData(cards);
-    };
+  fetchCards();
 
-    fetchCards();
-  }, [difficulty]);
-
-  return cardsData;
+  return cards;
 }
 
 export default useRandomCards;
