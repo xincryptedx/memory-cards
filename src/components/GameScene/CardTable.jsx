d0655c707fe268c94dc5120cc02b630323dd27d3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-09 12:25:02 -0400

    Add state for tracking chosen cards

--- src/components/GameScene/CardTable.jsx
+++ src/components/GameScene/CardTable.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
 import styles from "./CardTable.module.css";
 import Card from "./Card.jsx";
+import { useState } from "react";
 
 function CardTable({ cards }) {
+  const [chosenCards, setChosenCards] = useState([]);
+
   return (
     <section className={styles.cardTable}>
       {cards.map((card) => (
@@ -11,6 +14,8 @@ function CardTable({ cards }) {
           power={card.value}
           suit={card.suit}
           image={card.image}
+          chosenCards={chosenCards}
+          setChosenCards={setChosenCards}
         ></Card>
       ))}
     </section>
