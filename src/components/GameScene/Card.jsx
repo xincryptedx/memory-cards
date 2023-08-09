b9683749f7dc6559d44c39102443102faa02b42e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-09 12:35:45 -0400

    Add method outline for handling card clicks

--- src/components/GameScene/Card.jsx
+++ src/components/GameScene/Card.jsx
@@ -1,8 +1,30 @@
 /* eslint-disable react/prop-types */
 import styles from "./Card.module.css";
 
-function Card({ power, suit, image }) {
+function Card({
+  key,
+  power,
+  suit,
+  image,
+  score,
+  setScore,
+  chosenCards,
+  setChosenCards,
+  difficulty,
+  handleEndOfTurn,
+}) {
   const name = `${power} of ${suit}`;
+
+  const onChooseCard = () => {
+    // Check if the card is in chosenCards
+    if (chosenCards.includes(key)) {
+      // End the game
+    } else {
+      setScore((p) => p + 1);
+      setChosenCards((p) => [...p, key]);
+      handleEndOfTurn();
+    }
+  };
 
   return (
     <div className={styles.card}>
