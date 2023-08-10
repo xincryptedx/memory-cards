fa57adf3329d00007b2279955289ddb7d10800a9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-08-10 16:37:50 -0400

    Add help button

--- src/components/GameScene/ControlPanel.jsx
+++ src/components/GameScene/ControlPanel.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable react/prop-types */
 import styles from "./ControlPanel.module.css";
 
-function ControlPanel({ onNewGame, onQuit }) {
+function ControlPanel({ onNewGame, onQuit, setHelpModalOpen }) {
   return (
     <nav className={styles.controlPanel}>
       <button className={styles.newGameButton} onClick={onNewGame}>
         New Game
+      </button>
+      <button
+        className={styles.helpButton}
+        onClick={() => setHelpModalOpen(true)}
+      >
+        Help
       </button>
       <button className={styles.quitButton} onClick={onQuit}>
         Quit
